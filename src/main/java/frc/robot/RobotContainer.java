99d60fbfc0cd581a17328e213eb0a513f8c9a84a
FRC4728 <rocorirobotics@gmail.com>
2026-02-12 18:03:25 -0600

    Update RobotContainer.java

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -16,12 +16,15 @@ import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.RobotModeTriggers;
 import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine.Direction;
+import frc.robot.commands.RunIntakeIn;
 import frc.robot.commands.RunTurret;
 import frc.robot.generated.TunerConstants;
 import frc.robot.subsystems.CommandSwerveDrivetrain;
+import frc.robot.subsystems.Intake;
 import frc.robot.subsystems.Turret;
 
 public class RobotContainer {
+    private Intake intake = new Intake();
     private Turret turret = new Turret();
     private double MaxSpeed = 1.0 * TunerConstants.kSpeedAt12Volts.in(MetersPerSecond); // kSpeedAt12Volts desired top speed
     private double MaxAngularRate = RotationsPerSecond.of(0.75).in(RadiansPerSecond); // 3/4 of a rotation per second max angular velocity
@@ -80,6 +83,8 @@ public class RobotContainer {
 
         joystick2.a().whileTrue(new RunTurret(turret));
         joystick2.a().whileFalse(new RunTurret(turret));
+
+        joystick.x().onTrue(new RunIntakeIn(intake));
 
         drivetrain.registerTelemetry(logger::telemeterize);
     }
