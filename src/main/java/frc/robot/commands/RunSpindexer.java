1b659e627d8a74912385f910eae23b646841483b
FRC4728 <rocorirobotics@gmail.com>
2026-02-21 09:15:03 -0600

    Speeds

--- src/main/java/frc/robot/commands/RunSpindexer.java
+++ src/main/java/frc/robot/commands/RunSpindexer.java
@@ -27,7 +27,7 @@ public class RunSpindexer extends Command {
   @Override
   public void execute(){
     //theoretically run indexer whatever direction it's supposed to go
-    indexer.runIndexer(90);
+    indexer.runIndexer(80);
   }
 
   @Override
