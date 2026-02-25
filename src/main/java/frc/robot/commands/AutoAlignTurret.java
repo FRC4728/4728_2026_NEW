c8152d91c82d55ea0fd64050b855065a596d3f45
FRC4728 <rocorirobotics@gmail.com>
2026-02-25 17:21:13 -0600

    Weird Auto Align?

--- src/main/java/frc/robot/commands/AutoAlignTurret.java
+++ src/main/java/frc/robot/commands/AutoAlignTurret.java
@@ -1,11 +1,8 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
-
 package frc.robot.commands;
 
 import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.Constants;
 import frc.robot.LimelightHelpers;
@@ -22,25 +19,32 @@ public class AutoAlignTurret extends Command {
 
   @Override
   public void initialize() {
-    // Switch to the AprilTag/target pipeline
     NetworkTableInstance.getDefault().getTable("limelight-turret").getEntry("pipeline").setDouble(0);
+    System.out.println("AutoAlignTurret: command started");
   }
 
   @Override
   public void execute() {
-    if (LimelightHelpers.getTV("limelight-turret")) {
+    boolean hasTarget = LimelightHelpers.getTV("limelight-turret");
+    double tx = LimelightHelpers.getTX("limelight-turret");
+
+    double voltage = MathUtil.clamp(
+        tx * Constants.TurretConstants.k_ll_kP,
+        -Constants.TurretConstants.k_ll_maxVoltage,
+        Constants.TurretConstants.k_ll_maxVoltage
+    );
+
+    SmartDashboard.putBoolean("Turret/CommandRunning", true);
+    SmartDashboard.putNumber("Turret/CommandVoltage", voltage);
+    SmartDashboard.putBoolean("Turret/CommandHasTarget", hasTarget);
+
+    if (hasTarget) {
       if (!m_turret.isAligned()) {
-        double voltage = LimelightHelpers.getTX("limelight-turret") * Constants.TurretConstants.k_ll_kP;
-        voltage = MathUtil.clamp(voltage,
-            -Constants.TurretConstants.k_ll_maxVoltage,
-            Constants.TurretConstants.k_ll_maxVoltage);
         m_turret.moveTurretVoltage(voltage);
       } else {
-        // On target — hold still
         m_turret.stopTurretVoltage();
       }
     } else {
-      // No target visible — don't move
       m_turret.stopTurretVoltage();
     }
   }
@@ -48,6 +52,9 @@ public class AutoAlignTurret extends Command {
   @Override
   public void end(boolean interrupted) {
     m_turret.stopTurretVoltage();
+    SmartDashboard.putBoolean("Turret/CommandRunning", false);
+    SmartDashboard.putNumber("Turret/CommandVoltage", 0);
+    System.out.println("AutoAlignTurret: command ended, interrupted=" + interrupted);
   }
 
   @Override
