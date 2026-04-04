a1b5b577653dfdf42d7026ac7914aea1982627aa
FRC4728 <rocorirobotics@gmail.com>
2026-04-04 13:07:54 -0500

    Shooter Table New #

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -91,7 +91,7 @@ public class RobotContainer {
     /* Path follower */
     private final SendableChooser<Command> autoChooser;
     public void periodic(){
-        
+
     }
 
     public RobotContainer() {
@@ -153,7 +153,7 @@ public class RobotContainer {
         //driver.leftBumper().whileTrue(new RunIntakeIn(intake));
 
         driver.rightBumper().whileTrue(new Score(indexer, kicker, shooter, turret, drivetrain).withInterruptBehavior(Command.InterruptionBehavior.kCancelSelf));
-        driver.leftBumper().whileTrue(new EmergencyScore(intake, indexer, kicker, shooter, turret).withInterruptBehavior(Command.InterruptionBehavior.kCancelSelf));
+        driver.leftBumper().whileTrue(new ScoreDyn(intake, indexer, kicker, shooter, turret).withInterruptBehavior(Command.InterruptionBehavior.kCancelSelf));
         driver.a().whileTrue(new RunSpindexerRev(indexer));
         driver.start().whileTrue(new ReverseAll(kicker, intake, indexer));
         driver.y().onTrue(drivetrain.runOnce(drivetrain::seedFieldCentric));
