9650e305a9e18e878d5e015cddf02d934c35d66a
FRC4728 <rocorirobotics@gmail.com>
2026-04-15 16:14:00 -0500

    Created Many more autos and lowerd score command auto time

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -96,9 +96,9 @@ public class RobotContainer {
         new EventTrigger("DropIntake").onTrue(new DropIntake(intake));
         new EventTrigger("ZeroTurret").onTrue(new SetTurretZeroish(turret));
         new EventTrigger("CenterTurret").onTrue(new SetTurretCenter(turret));
-        new EventTrigger("Score").whileTrue(new Score(indexer, kicker, shooter, turret, drivetrain).withTimeout(9));
+        new EventTrigger("Score").whileTrue(new Score(indexer, kicker, shooter, turret, drivetrain).withTimeout(8));
 
-        NamedCommands.registerCommand("Score",new Score(indexer, kicker, shooter, turret, drivetrain).withTimeout(9));
+        NamedCommands.registerCommand("Score",new Score(indexer, kicker, shooter, turret, drivetrain).withTimeout(8));
         NamedCommands.registerCommand("RunIntake",new RunIntakeIn(intake).withTimeout(20));
  
         //create auto chooser in dashboard
